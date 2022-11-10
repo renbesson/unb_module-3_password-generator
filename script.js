f6463921090f502bfdca19ead0880c0c57cc98a4
Ren Besson <ren.besson@outlook.com>
2022-11-10 00:47:59 -0400

    Changed to do/while loop

--- script.js
+++ script.js
@@ -2,48 +2,42 @@
 var generateBtn = document.querySelector("#generate");
 
 var generatePassword = function () {
+
   // Password length (>= 8 && <= 128)
-  var pswdLength = prompt(
-    "What is the password length you desire?\nRemembering that it should be between 8 and 128 characteres."
-  );
-  pswdLength = Number(pswdLength);
-
-  while (isNaN(pswdLength) || pswdLength < 8 || pswdLength > 128) {
+  do {
+    var pswdLength = prompt(
+      "What is the password length you desire?\nRemembering that it should be between 8 and 128 characteres."
+    );
     pswdLength = Number(pswdLength);
-    pswdLength = prompt("Please select a number between 8 and 128.");
-  }
-  // Includes lowercase?
-  var pswdLowercase = confirm(
-    "Shall I include lowercase letters to your password?"
-  );
+  } while (isNaN(pswdLength) || pswdLength < 8 || pswdLength > 128);
 
-  // Includes uppercase?
-  var pswdUppercase = confirm("How about uppercase letters?");
-
-  // Includes numbers?
-  var pswdNumbers = confirm(
-    "Numbers should be included in the criteria as well?"
-  );
-
-  // Includes special characters?
-  var pswdSpecialCharacters = confirm(
-    "The password will be more secure if it has special characters.\nShould I include any?"
-  );
-
-  while (
-    !pswdLowercase ||
-    !pswdUppercase ||
-    !pswdNumbers ||
-    !pswdSpecialCharacters
-  ) {
+  // Criteria questions
+  do {
     alert("Please select at least one criteria.");
-    pswdLowercase = confirm(
+
+    // Includes lowercase?
+    var pswdLowercase = confirm(
       "Shall I include lowercase letters to your password?"
     );
-    pswdUppercase;
-    pswdNumbers;
-    pswdSpecialCharacters;
-  }
+
+    // Includes uppercase?
+    var pswdUppercase = confirm("How about uppercase letters?");
+
+    // Includes numbers?
+    var pswdNumbers = confirm(
+      "Numbers should be included in the criteria as well?"
+    );
+
+    // Includes special characters?
+    var pswdSpecialCharacters = confirm(
+      "The password will be more secure if it has special characters.\nShould I include any?"
+    );
+  } while (
+    !pswdLowercase &&
+    !pswdUppercase &&
+    !pswdNumbers &&
+    !pswdSpecialCharacters
+  );
 };
 
 // Write password to the #password input
