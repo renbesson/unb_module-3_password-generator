3604f713471594c31df43c23a83adb5779ac53f3
Ren Besson <ren.besson@outlook.com>
2022-11-10 02:36:12 -0400

    Logic finished

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var generatePassword = function () {
-
+var questions = function () {
   // Password length (>= 8 && <= 128)
   do {
     var pswdLength = prompt(
@@ -38,6 +37,54 @@ var generatePassword = function () {
     !pswdNumbers &&
     !pswdSpecialCharacters
   );
+
+  return {
+    pswdLength,
+    pswdLowercase,
+    pswdUppercase,
+    pswdNumbers,
+    pswdSpecialCharacters,
+  };
+};
+
+var getChar = function (type) {
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var special = "!@#$%^&*()-_=+";
+
+  if (type === "lowercase") {
+    var position = Math.floor(Math.random() * alphabet.length);
+    return alphabet[position];
+  } else if (type === "uppercase") {
+    var position = Math.floor(Math.random() * numbers.length);
+    return alphabet.toUpperCase()[position];
+  } else if (type === "number") {
+    var position = Math.floor(Math.random() * numbers.length);
+    return numbers[position];
+  } else if (type === "special") {
+    var position = Math.floor(Math.random() * special.length);
+    return special[position];
+  }
+};
+
+var randomType = function (answers) {
+  do {
+    var num = Math.floor(Math.random() * 4);
+    if (num === 0 && answers.pswdLowercase === true) return "lowercase";
+    if (num === 1 && answers.pswdUppercase === true) return "uppercase";
+    if (num === 2 && answers.pswdNumbers === true) return "number";
+    if (num === 3 && answers.pswdSpecialCharacters === true) return "special";
+  } while (true);
+};
+
+var generatePassword = function () {
+  var newPassword = "";
+  var answers = questions();
+
+  for (i = 1; i <= answers.pswdLength; i++) {
+    newPassword += getChar(randomType(answers));
+  }
+  return newPassword;
 };
 
 // Write password to the #password input
